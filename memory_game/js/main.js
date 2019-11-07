830e484f06f91f7834c37ef4991eeb80d4546a84
Ellen Waddell <ellenwaddell@Ellens-MacBook-Pro.local>
2019-11-07 13:55:27 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,27 @@
 
 const cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cards[0]);
-console.log("User flipped queen.");
+const cardsInPlay = [];
 
 
-var cardTwo = cards[2];
-cardsInPlay.push(cards[2]);
-console.log("User flipped king");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match"); 
+		} else {
+			console.log("Sorry, try again");
+		}
+	}
+};
 
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match"); 
-} else {
-	alert("Sorry, try again");
-}
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
 
